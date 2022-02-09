a6adb27dc01eb49352b20b781c77f5b36b4849ff
Matthias Iacsa <mail@matthias.iacsa.de>
2022-02-09 07:25:57 +0100

    [feat] new solution: scrabble-score (rust)

---
+++ rust/scrabble-score/src/lib.rs
@@ -0,0 +1,16 @@
+fn value(c: char) -> u32 {
+    match c {
+        'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+        'D' | 'G' => 2,
+        'B' | 'C' | 'M' | 'P' => 3,
+        'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+        'K' => 5,
+        'J' | 'X' => 8,
+        'Q' | 'Z' => 10,
+        _ => 0,
+    }
+}
+
+pub fn score(word: &str) -> u32 {
+    word.chars().map(|c| value(c.to_ascii_uppercase())).sum()
+}
